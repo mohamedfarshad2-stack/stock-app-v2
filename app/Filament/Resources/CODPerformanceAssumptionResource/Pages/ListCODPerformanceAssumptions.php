aa2506c7828431dba4deffbba57e5fc6434b4ff2
mohamedfarshad2-stack <mohamedfarshad2@gmail.com>
2026-05-17 11:41:09 +0530

    Add HALOS Phase 1 import center contract for stabilization roadmap

---
+++ app/Filament/Resources/CODPerformanceAssumptionResource/Pages/ListCODPerformanceAssumptions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\CODPerformanceAssumptionResource\Pages;
+
+use App\Filament\Resources\CODPerformanceAssumptionResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListCODPerformanceAssumptions extends ListRecords
+{
+    protected static string $resource = CODPerformanceAssumptionResource::class;
+}
